4ce559fe2c191f523d908627fc888a61859696b2
ryan.pina-silasse <ryan.pina-silasse@konbini.com>
2022-11-23 15:50:03 +0100

    CSS added

--- src/components/profil/Infos.js
+++ src/components/profil/Infos.js
@@ -6,7 +6,6 @@ const Infos = () => {
       <h2 className="text-5xl font-sans font-bold text-yellow-400 p-12 text-center underline-offset-8">
         5 infos vite fait, bien fait !
       </h2>
-      <FontAwesomeIcon icon="fa-solid fa-angle-right bg-red-800" />
 
       <div className="grid grid-cols-2 gap-8 bg-blue-700 p-8 rounded-xl">
         {datas && // Vérification de l'existence de la variable et de sa longueur
@@ -15,11 +14,14 @@ const Infos = () => {
             //Puis parcours du tableau
 
             return (
-              <div key={index} className="bg-white rounded-xl p-4">
-                <h2 className="text-2xl text-black font-bold">
+              <div
+                key={index}
+                className="tableau-infos bg-white rounded-xl p-4"
+              >
+                <h2 className="tableau-title text-2xl font-bold">
                   {element.title}
                 </h2>
-                <p className="text-black text-x ">{element.category}</p>
+                <p className="tableau-category text-x ">{element.category}</p>
               </div>
             );
           })}
