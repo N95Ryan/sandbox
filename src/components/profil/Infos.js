3b9cd5d7af4e1e52bf809e13b98f9ff474b503c4
ryan.pina-silasse <ryan.pina-silasse@konbini.com>
2022-11-23 12:41:06 +0100

    FontAwesome added

--- src/components/profil/Infos.js
+++ src/components/profil/Infos.js
@@ -1,11 +1,12 @@
 import datas from "./Datas";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Infos = () => {
   return (
     <div>
       <h2 className="text-5xl font-sans font-bold text-yellow-400 p-12 text-center underline-offset-8">
         5 infos vite fait, bien fait !
       </h2>
+      <FontAwesomeIcon icon="fa-solid fa-angle-right bg-red-800" />
 
       <div className="grid grid-cols-2 gap-8 bg-blue-700 p-8 rounded-xl">
         {datas && // Vérification de l'existence de la variable et de sa longueur
